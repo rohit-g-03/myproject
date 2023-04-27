9280c8b8e3af96151b58ca84470e3ba6e3a0fa37
rohitgite03 <rohitgite03@gmail.com>
2023-04-28 00:06:31 +0530

    Second Commit

--- helloworld.c
+++ helloworld.c
@@ -2,7 +2,7 @@
 
 int main(){
 printf("Hell Everyone! This is my project, F*ck all of U!");
-printf("Forgot to maintion my name, I'm Rohit.");
+printf("Forgot to maintion my name, I'm Rohit, we ae working on the same project");
 
 return 0;
 }
